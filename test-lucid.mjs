5aade7aa2a4ea895d98aa016ab0988d5e7392454
dpegoli <dpegoli@gmail.com>
2025-03-10 00:31:11 -0400

    Updated Lucid test files and dependencies

--- test-lucid.mjs
+++ test-lucid.mjs
@@ -1,1 +1,14 @@
 import { Lucid } from "lucid-cardano"; console.log("Lucid loaded:", Lucid);
+
+
+async function main() {
+  const lucid = await Lucid.new(
+    new Blockfrost("https://cardano-preprod.blockfrost.io/api/v0", "previewmYOKn2ziEcTZw4GFtQo3qGwNjRfCiAIW"),
+    "Prepod"
+  );
+
+  console.log("✅ Lucid is connected!");
+  console.log("🌐 Network:", await lucid.network());
+}
+
+main().catch(console.error);
